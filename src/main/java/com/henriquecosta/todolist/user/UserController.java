8388592f68f87d201cd0a1ba540f126a4ef76fe7
Henrique Costa <henrique.costa@flowih.com>
2025-03-13 20:43:32 -0300

    feat: implementação do repositório de usuários e atualização do controlador para verificar a existência de usuários antes da criação

--- src/main/java/com/henriquecosta/todolist/user/UserController.java
+++ src/main/java/com/henriquecosta/todolist/user/UserController.java
@@ -1,15 +1,25 @@
 package com.henriquecosta.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/create")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.printf(
-        "As informações de usuário são as seguintes:\n\nName: %s\nUsername: %s\nPassword: %s",
-        userModel.getName(), userModel.getUsername(), userModel.getPassword());
+  public UserModel create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (user != null) {
+      System.out.println("Usuário já existe!");
+      return null;
+    }
+    var userCreated = this.userRepository.save(userModel);
+    return userCreated;
+
   }
 }
--- src/main/java/com/henriquecosta/todolist/user/UserModel.java
+++ src/main/java/com/henriquecosta/todolist/user/UserModel.java
@@ -1,11 +1,25 @@
 package com.henriquecosta.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+import org.hibernate.annotations.CreationTimestamp;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
 
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+
+  @Column(unique = true)
   private String username;
   private String name;
   private String password;
+
+  @CreationTimestamp
+  private LocalDateTime created_at;
+
 }
