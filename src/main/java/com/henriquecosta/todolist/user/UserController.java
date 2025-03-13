0826ad977f7d2b6cdd69d53dbb730d079df4125b
Henrique Costa <henrique.costa@flowih.com>
2025-03-13 19:38:44 -0300

    feat: rota de criar usuário definida e apresentação desses dados no console

---
+++ src/main/java/com/henriquecosta/todolist/user/UserController.java
@@ -0,0 +1,20 @@
+package com.henriquecosta.todolist.user;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+  /**
+   * String (texto) Integer (int) numeros inteiros Double (double) numberos 0.000 Float (float)
+   * numeros 0.00 chat (A C) Date (data) void (sem retorno)
+   */
+
+  @PostMapping("/create")
+  public void create(@RequestBody UserModel userModel) {
+    System.out.printf(
+        "As informações de usuário são as seguintes:\n\nName: %s\nUsername: %s\nPassword: %s",
+        userModel.name, userModel.username, userModel.password);
+  }
+}
