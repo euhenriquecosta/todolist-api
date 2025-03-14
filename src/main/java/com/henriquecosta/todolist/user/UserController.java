70e67b70f8eebb14f6e566eea8d19d5a715bd92f
Henrique Costa <henrique.costa@flowih.com>
2025-03-13 21:27:18 -0300

    feat: atualização do UserController para retornar ResponseEntity e criptografar senhas ao criar usuários

--- src/main/java/com/henriquecosta/todolist/user/UserController.java
+++ src/main/java/com/henriquecosta/todolist/user/UserController.java
@@ -1,7 +1,10 @@
 package com.henriquecosta.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
@@ -11,15 +14,21 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping("/create")
-  public UserModel create(@RequestBody UserModel userModel) {
+  public ResponseEntity create(@RequestBody UserModel userModel) {
     var user = this.userRepository.findByUsername(userModel.getUsername());
 
     if (user != null) {
       System.out.println("Usuário já existe!");
-      return null;
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe.");
     }
+
+    var passwordHashred =
+        BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+    userModel.setPassword(passwordHashred);
+
     var userCreated = this.userRepository.save(userModel);
-    return userCreated;
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
 
   }
 }
