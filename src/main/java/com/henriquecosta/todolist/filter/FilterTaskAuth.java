6620b8a0084c0b927c8b95d6770aca14a93973d0
Henrique Costa <henrique.costa@flowih.com>
2025-03-13 23:31:29 -0300

    feat: implementação do filtro de autenticação para validação de usuários em requisições de tarefas

---
+++ src/main/java/com/henriquecosta/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,70 @@
+package com.henriquecosta.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import com.henriquecosta.todolist.user.IUserRepository;
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+  @Autowired
+  private IUserRepository userRepository;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+
+    var servletPath = request.getServletPath();
+
+    if (servletPath.contains("/tasks/")) {
+      // Pegar a autorização
+
+      var authorization = request.getHeader("Authorization");
+
+      // Tira o Basic do código da autorização
+      var authEncoded = authorization.substring("Basic".length()).trim();
+
+      // Decodifica o código da autorização e transforma em string
+      byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+      var authString = new String(authDecoded);
+
+      // Divide em duas partes, usuario e senha
+      String[] credentials = authString.split(":");
+
+      String username = credentials[0];
+      String password = credentials[1];
+
+      System.out.println(username);
+      System.out.println(password);
+
+      // sValida o usuário
+
+      var user = this.userRepository.findByUsername(username);
+      if (user == null) {
+        response.sendError(401);
+      } else {
+        var result = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+        if (result.verified) {
+          filterChain.doFilter(request, response);
+        } else {
+          response.sendError(401);
+        }
+
+        // Segue viagem
+      }
+    } else {
+      filterChain.doFilter(request, response);
+    }
+
+  }
+
+}
