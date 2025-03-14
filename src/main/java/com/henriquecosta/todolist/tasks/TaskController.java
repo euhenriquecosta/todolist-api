5711f61adf293767ca244441b65757b43a4b6c2a
Henrique Costa <henrique.costa@flowih.com>
2025-03-14 01:32:38 -0300

    refactor: simplificação do método de atualização de tarefas utilizando a classe Utils para copiar propriedades nulas

--- src/main/java/com/henriquecosta/todolist/tasks/TaskController.java
+++ src/main/java/com/henriquecosta/todolist/tasks/TaskController.java
@@ -1,6 +1,5 @@
 package com.henriquecosta.todolist.tasks;
 
-import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -8,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.henriquecosta.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 
@@ -57,40 +57,10 @@ public class TaskController {
   @PutMapping("/update/{id}")
   public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request,
       @PathVariable UUID id) {
-    var idUser = (UUID) request.getAttribute("idUser");
+    var task = this.taskRepository.findById(id).orElse(null);
 
-    var tasks = this.taskRepository.findByIdUser(idUser);
-    var task = tasks.stream().filter(t -> t.getId().equals(id)).findFirst().orElse(null);
-
-    if (task == null) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND)
-          .body("Tarefa não encontrada ou não pertence ao usuário!");
-    }
-
-    if (!task.getIdUser().equals(idUser)) {
-      return ResponseEntity.status(HttpStatus.FORBIDDEN)
-          .body("Você não tem permissão para atualizar esta tarefa!");
-    }
-
-    updateNonNullFields(taskModel, task);
-
-    taskModel.setId(id);
-    taskModel.setIdUser((UUID) idUser);
+    Utils.copyNullProperties(taskModel, task);
 
     return ResponseEntity.status(HttpStatus.OK).body(this.taskRepository.save(task));
-  }
-
-  private void updateNonNullFields(Object source, Object target) {
-    for (Field field : source.getClass().getDeclaredFields()) {
-      field.setAccessible(true);
-      try {
-        Object value = field.get(source);
-        if (value != null) {
-          field.set(target, value);
-        }
-      } catch (IllegalAccessException e) {
-        System.err.printf("Erro ao acessar o campo: {}", field.getName(), e);
-      }
-    }
   }
 }
